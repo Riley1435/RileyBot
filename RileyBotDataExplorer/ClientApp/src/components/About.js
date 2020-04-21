662a35d94fc13ba8ca64e8115ad79f0a9c0ad3ce
Riley Marsh <Riley.marsh867@gmail.com>
2020-04-21 12:16:16 -0700

    Added sample command images and styles

--- RileyBotDataExplorer/ClientApp/src/components/About.js
+++ RileyBotDataExplorer/ClientApp/src/components/About.js
@@ -1,4 +1,8 @@
 ﻿import React, { Component } from 'react';
+import newuser from './images/newuser.png'
+import newdrop from './images/newdrop.png'
+import newlink from './images/newlink.png'
+import './About.css'
 
 export class About extends Component {
     static displayName = About.name;
@@ -6,10 +10,25 @@ export class About extends Component {
 
   render () {
     return (
-      <div>
+    <div>
         <h1>About</h1>
-            <p>This web application is used to explore and view the data of the RileyBot discord bot</p>
-      </div>
+            <p>This web application is used to explore and view
+                the data of the RileyBot discord bot</p>
+
+            <h2>Sample Commands</h2>
+
+            <h3>Adding a new user</h3>
+            <img src={newuser} alt="new user command"/>
+            <br />
+
+            <h3>Adding a new drop</h3>
+            <img src={newdrop} alt="new drop command"/>
+            <br />
+
+            <h3>Adding a new link</h3>
+            <img src={newlink} alt="new link command" />
+            <br />
+    </div>
     );
   }
 }
