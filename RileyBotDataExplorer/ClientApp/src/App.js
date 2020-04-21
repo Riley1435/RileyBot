0b6f02dc94ff8c9424b4ac829dde235370021336
Riley Marsh <Riley.marsh867@gmail.com>
2020-04-21 02:17:43 -0700

    Added about page and removed excess files

--- RileyBotDataExplorer/ClientApp/src/App.js
+++ RileyBotDataExplorer/ClientApp/src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { Counter } from './components/Counter';
+import { About } from './components/About';
 import { UserComponent } from './components/UserComponent';
 import { DropComponent } from './components/DropComponent';
 import { LinkComponent } from './components/LinkComponent';
@@ -16,6 +17,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
         <Route path='/counter' component={Counter} />
         <Route path='/user-data' component={UserComponent} />
         <Route path='/drop-data' component={DropComponent} />
---
+++ RileyBotDataExplorer/ClientApp/src/components/About.js
@@ -0,0 +1,15 @@
+﻿import React, { Component } from 'react';
+
+export class About extends Component {
+    static displayName = About.name;
+
+
+  render () {
+    return (
+      <div>
+        <h1>About</h1>
+            <p>This web application is used to explore and view the data of the RileyBot discord bot</p>
+      </div>
+    );
+  }
+}
